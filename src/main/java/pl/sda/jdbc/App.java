d07af84af597db9defe348e100a0361f52e2aa15
PawelKonczalski <konczalski.pawel@gmail.com>
2019-08-21 21:32:29 +0200

    first commit

---
+++ src/main/java/pl/sda/jdbc/App.java
@@ -0,0 +1,15 @@
+package pl.sda.jdbc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+        SessionFactory sf = new Configuration()
+                .configure()
+                .buildSessionFactory();
+
+        Session session = sf.openSession();
+    }
+}
