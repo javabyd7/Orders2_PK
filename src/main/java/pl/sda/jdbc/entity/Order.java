be3b4d07d1be1f600105f5be1263b55db95803fe
PawelKonczalski <konczalski.pawel@gmail.com>
2019-08-21 22:58:31 +0200

    tables generate

---
+++ src/main/java/pl/sda/jdbc/entity/Order.java
@@ -0,0 +1,77 @@
+package pl.sda.jdbc.entity;
+
+import javax.persistence.*;
+import java.util.Arrays;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "products")
+    private String[] products;
+
+    @Column(name = "order_date")
+    private String orderDate;
+
+    public Order() {
+    }
+
+    public Order(User user, String[] products, String orderDate) {
+        this.user = user;
+        this.products = products;
+        this.orderDate = orderDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(String[] products) {
+        this.products = products;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", user=" + user +
+                ", products=" + Arrays.toString(products) +
+                ", orderDate='" + orderDate + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pl/sda/jdbc/entity/User.java
@@ -0,0 +1,73 @@
+package pl.sda.jdbc.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "adres")
+    private String adres;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String adres) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adres = adres;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", adres='" + adres + '\'' +
+                '}';
+    }
+}
